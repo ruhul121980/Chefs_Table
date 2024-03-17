fb7111309f8c5162083a1edc1835121b80b576a5
unknown <ruhul121980@gmail.com>
2024-03-18 05:06:09 +0600

    second commit

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,10 +1,11 @@
 const Banner = () => {
     return (
-      <div className="bg-cover bg-center flex items-center justify-center h-screen" style={{backgroundImage: "url('/public/images/Rectangle 1.png')"}}>
+      <div className="bg-cover bg-center flex items-center justify-center h-screen" style={{backgroundImage: "url('https://i.postimg.cc/xj51PbGz/Rectangle.png')"}}>
         <div className="text-center text-white">
           <h1 className="text-4xl font-bold"> Special cooking class tailored for you!</h1>
           <p className="mt-4">"Special cooking" typically refers to the preparation of dishes that are distinct from everyday meals</p>
         </div>
+        
       </div>
     );
   }
